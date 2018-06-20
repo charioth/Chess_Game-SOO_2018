7c7f609471ea1b653e91b0dbd752e488bb7cdf2b
BrunoMarchi <msc1376@gmail.com>
2018-06-20 03:53:24 +0000

    added class diagrams

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,10 +2,9 @@
 import models.game.Game;
 
 public class Launcher {
-	public static void main(String[] args) throws  Exception
-	{
-		Game game  = new Game("The Last Chess Game", 0.6f); //Change the number to alter the screen size
-		
-		game.start();	
+	public static void main(String[] args) throws Exception {
+		Game game = new Game("The Last Chess Game", 0.6f); // Change the number to alter the screen size
+
+		game.start();
 	}
 }
